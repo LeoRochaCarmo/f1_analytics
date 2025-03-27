b3e9d180054aad8716355150f2c617b04353ecb4
Leonardo <leorochaengenharia@gmail.com>
2025-03-27 11:07:25 -0300

    reorganizing files

--- pipeline_csv_to_db/pipeline.py
+++ pipeline_csv_to_db/pipeline.py
@@ -1,7 +1,6 @@
 import kaggle_download
 import db_schema
 import load_data
-from time import sleep
 def main():
     print('Pipeline of F1 data begins...')
     kaggle_download.moving_files()
@@ -9,4 +8,5 @@ def main():
     load_data.load_csv_to_db()
     print('Pipeline has ended.')
 
-main()
+if __name__ == '__main__':
+    main()
